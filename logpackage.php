af91d6593f62a93a690655c4ef83e707f44d11f0
Phillip Corley <pcorl311@live.kutztown.edu>
2021-02-22 15:16:13 -0500

    Added search student function to mail logging

--- logpackage.php
+++ logpackage.php
@@ -24,7 +24,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "test";
-
+$email = "";
 if (!isset ($_POST['trackingID']))
 {
     header ('Location: index.php');
@@ -34,24 +34,71 @@ try{
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+  
+  // search student db for record of student and retrieve email and other residential information
+  $query = "SELECT email FROM STUDENT 
+  WHERE `name_last` = :name_last AND `name_first` = :name_first AND `building` = :building
+  AND `room_num` = :room_num AND `bed_letter` = :bed_letter"; 
+  $search = $conn->prepare($query);
+  
   // prepare sql and bind parameters
-  $stmt = $conn->prepare("INSERT INTO PACKAGE (building, log_date, name_first, name_last, tracking_ID)
-  VALUES (:building, NOW(), :name_first, :name_last, :tracking_ID)");
+  $search->bindParam(':name_last', $_POST['nameLast']);
+  $search->bindParam(':name_first', $_POST['nameFirst']);
+  $search->bindParam(':building', $_POST['building']);
+  $search->bindParam(':room_num', $_POST['roomNum']);
+  $search->bindParam(':bed_letter', $_POST['bedLetter']);
   
-  $stmt->bindParam(':building', $_POST['building']);
-  $stmt->bindParam(':name_first', $_POST['nameFirst']);
-  $stmt->bindParam(':name_last', $_POST['nameLast']);
-  $stmt->bindParam(':tracking_ID', $_POST['trackingID']);
+  $search->execute();
   
-  $stmt->execute();
+  $return = $search->fetchall(PDO::FETCH_ASSOC);
+  
+  // if student record was found in database, pull email and log package into package database
+  if ($search->rowCount() == 1) {
+	// get email from search query
+	$email = $return[0]['email'];
+    
+	// prepare sql and bind parameters	
+	$stmt = $conn->prepare("INSERT INTO PACKAGE (building, log_date, name_first, name_last, tracking_ID)
+    VALUES (:building, NOW(), :name_first, :name_last, :tracking_ID)");
+  
+    $stmt->bindParam(':building', $_POST['building']);
+    $stmt->bindParam(':name_first', $_POST['nameFirst']);
+    $stmt->bindParam(':name_last', $_POST['nameLast']);
+    $stmt->bindParam(':tracking_ID', $_POST['trackingID']);
+  
+    $stmt->execute();
 
-  echo "New records created successfully. ";
+    echo "An email will be sent to: ".$email."<br><br>";
+    echo "New records created successfully.<br><br>";
+  
+  } else {
+	/* 
+	  This should echo an error that there is no student in the database 
+	  that matches these records and forces the user to input the data in 
+	  again or search to see if the student is still at that residence 
+	  using THD or other residential software.
+	  */
+	  echo "Student not Found<br><br>";
+	  echo "<button onclick=\"goBack()\">Go Back</button>
+
+			<script>
+			function goBack() {
+				window.history.back();
+			}
+			</script>
+			<form action=\"https:\/\/mykuhousing.kutztown.edu/mobile\">
+				<input type=\"submit\" value=\"Lookup Student\" />
+			</form>";
+	  
+	//echo "<p>ERROR</p>";
+	//header ('Location: index.php');
+	die();
+  }
 } catch(PDOException $e) {
   echo "Error: " . $e->getMessage();
 }
 $conn = null;
-$email = $_POST['email'];
+
 $trackingID = $_POST['trackingID'];
 
 //echo "<form action='print.php'method='post' id = 'auto'> <input type = 'hidden' name = 'email' value = '{$email}'> <input type = 'hidden' name = 'trackingID' value = '{$trackingID}'> <input type = 'submit'></form>";
@@ -59,8 +106,6 @@ $trackingID = $_POST['trackingID'];
 $sName = $_POST['nameFirst'];
 // call notifyStudent. pass the email and tracking id to be sent in the email
 echo (notifyStudent($email, $trackingID, $sName)) ? " Notification sent" : " Error sending email.";
-
-
 
 ?>
 
