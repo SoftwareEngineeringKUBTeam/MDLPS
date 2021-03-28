3f235cc987ba2ca6442be1d000d356f13023b0e2
NickN96 <71345987+NickN96@users.noreply.github.com>
2021-03-28 00:36:18 -0400

    Update login.php

--- login.php
+++ login.php
@@ -40,7 +40,7 @@ try {
     
     if(isset($_POST["username"]) && isset($_POST["password"])) {
 
-        $sql = ("SELECT * FROM logininfo WHERE user = :user AND pass = :passwd");
+        $sql = ("SELECT * FROM logininfo WHERE user = :user AND BINARY pass = :passwd");
         //$search = $conn->prepare($sql);
 
         //get fields from form
