1ce9651b0f24c97962bac2fb9a1a1b2d8eebd995
NickN96 <71345987+NickN96@users.noreply.github.com>
2021-04-19 18:55:55 -0400

    Update report.php

--- report.php
+++ report.php
@@ -69,9 +69,21 @@ report.php
             <form method="post" action="report.php">
                 <label>Range start:<input type="date" name="dFrom" placeholder="MM/DD/YYYY" required autofocus></label>
                 <label>Range end:<input type="date" name="dTo" placeholder="MM/DD/YYYY" required></label>
-
-
+                <p>Package Pickup Status</p>
+                <select type="text" name="PUstatus">
+                    <option value="all">All</option>
+                    <option value="npu">Package Not Picked Up</option>
+                    <option value="pu">Package Picked Up</option>
+                </select>
+                <div>
+                    <p>Which buildings would you like to include on the report?</p>
+                    <label>1<input class="line" type="checkbox" id="bldg1" name="bldg1" value="Building 1" checked></label>
+                    <label>2<input class="line" type="checkbox" id="bldg2" name="bldg2" value="Building 2" checked></label>
+                    <label>3<input class="line" type="checkbox" id="bldg3" name="bldg3" value="Building 3" checked></label>
+                    <label>4<input class="line" type="checkbox" id="bldg4" name="bldg4" value="Building 4" checked></label>
+                </div>
                 <input type="submit" value="Generate">
+                <input type="reset" value="Clear Form">
             </form>
             <div style="font-size:12px; color:red; margin-top:10px">
                 <?php if(isset($invalid)){echo $invalid;}?>
