79fc1d7d22bd3b82b502f17483760c57afeab7c3
chris-droney <71346115+chris-droney@users.noreply.github.com>
2021-04-19 10:49:51 -0400

    Update functions.php
    escaped some quotes that borked things

--- functions.php
+++ functions.php
@@ -60,7 +60,7 @@ function printFormTable($data) {
         $values = array_values($record);
         $form_value = implode(',', $values);
         print "<tr>";
-        print "<td><input type="submit" value ="Check Out"></td>";
+        print "<td><input type=\"submit\" value =\"Check Out\"></td>";
         foreach ($values as $v) {
             print "<td>$v</td>";
         }
