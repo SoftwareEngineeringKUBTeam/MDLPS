0182c050686332577e67ac9355a339dca659365e
chris-droney <71346115+chris-droney@users.noreply.github.com>
2021-04-19 12:14:42 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -63,7 +63,7 @@ function printPackageTable($data) {
 
         print "<td>";
         print "<form action='#' method='post'>";
-        print "<input type=\"hidden\" name=\"package_ID\" value = $form_value>";
+        print "<input type=\"hidden\" name=\"package_ID\" value = $record>";
         print "<input type=\"submit\" value =\"Check Out\"> </form></td>";
         foreach ($values as $v) {
             print "<td>$v</td>";
