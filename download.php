6e8498d8f4b9ae327767a75f8a96e69ce7196287
Hunter DeBlase <HunterDeBlase@gmail.com>
2021-04-21 15:00:23 -0400

    Create download.php

---
+++ download.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type: application/csv');
+header('Content-Disposition: attachment; filename="report.csv"');
+
+ob_end_clean();
+
+//Open file
+$handle = fopen('php://output', 'w');
+$report = $_POST["report"];
+
+//Output header line
+fputcsv($file, array_keys($report));
+
+//Ouput each line of report as csv line
+foreach ($report as $line) {
+    fputcsv($handle, $line);
+}
+
+//Clean up file and close
+fclose($file);
+ob_flush();
+header("Location: report.php");
+exit();
+?>
